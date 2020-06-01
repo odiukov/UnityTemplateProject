4fc686afba207ba1453d7be288f34945e8e2c2b2
AlexanderDykov <alexander.dykov@gmail.com>
2020-06-01 16:33:27 +0300

    add: analytic base interface

---
+++ Assets/Sources/DuckLib/Analytics/IAnalyticService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DuckLib.Analytics
+{
+    public interface IAnalyticService
+    {
+        void LogEvent(string eventName, IDictionary<string, object> eventData = null);
+    }
+}
