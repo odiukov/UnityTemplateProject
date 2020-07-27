deebdc80e7f566b65ce175d76492da6edaba23e9
AlexanderDykov <alexander.dykov@gmail.com>
2020-07-27 16:41:40 +0300

    fix: add return value for blueprint context extensions

--- Assets/Sources/DuckLib/Core/Extensions/ContextExtensions.cs
+++ Assets/Sources/DuckLib/Core/Extensions/ContextExtensions.cs
@@ -5,16 +5,17 @@ namespace DuckLib.Core.Extensions
 {
     public static class ContextExtensions
     {
-        public static void CreateEntity<TEntity>(this IContext<TEntity> context, IBlueprint<TEntity> blueprint)
+        public static TEntity CreateEntity<TEntity>(this IContext<TEntity> context, IBlueprint<TEntity> blueprint)
             where TEntity : class, IEntity
         {
-            blueprint.Apply(context.CreateEntity());
+            return context.CreateEntity().Apply(blueprint);
         }
 
-        public static void Apply<TEntity>(this TEntity entity, IBlueprint<TEntity> blueprint)
+        public static TEntity Apply<TEntity>(this TEntity entity, IBlueprint<TEntity> blueprint)
             where TEntity : class, IEntity
         {
             blueprint.Apply(entity);
+            return entity;
         }
     }
 }
